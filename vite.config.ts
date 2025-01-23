705a58652f631b5326ff4e7d45e5e65f9c65819e
Egon Zemmer <egon.zemmer@phlegx.com>
2025-01-23 23:20:56 +0100

    Fix Vite build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,53 @@
+import { defineConfig } from "vite";
 import path from "path";
-import {defineConfig} from "vite";
+import fg from "fast-glob";
+import dts from "vite-plugin-dts";
 
-module.exports = defineConfig({
+const inputFiles = Object.fromEntries(
+   fg.sync('src/**/*.ts').map(file => [
+       /* Remove `src/` and `.ts` extension for the output path */
+       file.slice(4, -3),
+       path.resolve(__dirname, file)
+   ])
+);
+
+export default defineConfig({
     base: "./",
+    plugins: [dts()],
     build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/index.ts"),
-            name: 'Uppload',
-            formats: ['es', 'cjs', 'umd', 'iife'],
-            fileName: 'uppload',
-        },
         rollupOptions: {
+            input: inputFiles,
             external: ["focus-trap", "mitt", "cropperjs"],
-            output: {
-                globals: {
-                    "focus-trap": "createFocusTrap",
-                    mitt: "mitt",
-                    cropperjs: "Cropper",
+            preserveEntrySignatures: 'exports-only',
+            output: [
+                {
+                    format: 'es', // ES Modules
+                    entryFileNames: '[name].js',
+                    chunkFileNames: '[name]-[hash].js',
+                    assetFileNames: '[name]-[hash].[ext]',
+                    sourcemap: true,
+                    exports: 'named',
+                    globals: {
+                        'focus-trap': 'createFocusTrap',
+                        mitt: 'mitt',
+                        cropperjs: 'Cropper',
+                    },
                 },
-            },
+                {
+                    format: 'cjs', // CommonJS Modules
+                    entryFileNames: '[name].cjs',
+                    chunkFileNames: '[name]-[hash].cjs',
+                    assetFileNames: '[name]-[hash].[ext]',
+                    sourcemap: true,
+                    exports: 'named',
+                    globals: {
+                        'focus-trap': 'createFocusTrap',
+                        mitt: 'mitt',
+                        cropperjs: 'Cropper',
+                    },
+                },
+            ],
         },
+        minify: true,
     },
 });
